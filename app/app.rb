929b70d3d1876df0af1ada91a953fae4d02513aa
Jonathan Pickard <Jonathandpickard@gmail.com>
2016-02-13 22:45:29 +0000

    added bcrypt

--- app/app.rb
+++ app/app.rb
@@ -5,6 +5,20 @@ require_relative './data_mapper_setup'
 
 class BookmarkManager < Sinatra::Base
   enable :sessions
+  set :session_secret, 'super_secret'
+
+  helpers do
+    def current_user
+      @current_user ||= User.get(session[:user_id])
+    end
+  end
+
+  post '/users' do
+    user = User.create(email: params[:email],
+                       password: params[:password])
+    session[:user_id] = user.id
+    redirect to('/links')
+  end
 
   get '/links' do
     @links = Link.all
