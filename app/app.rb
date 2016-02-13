6d830a42d876936d61fc38efa3f20d51f106ad75
Jonathan Pickard <Jonathandpickard@gmail.com>
2016-02-13 21:29:22 +0000

    added rakefile

--- app/app.rb
+++ app/app.rb
@@ -1,8 +1,6 @@
 ENV['RACK_ENV'] ||= "development"
 
 require          'sinatra/base'
-#require          'shotgun'
-#require_relative './models/link'
 require_relative './data_mapper_setup'
 
 class BookmarkManager < Sinatra::Base
@@ -18,12 +16,18 @@ class BookmarkManager < Sinatra::Base
   end
 
   post '/links' do
-    link = Link.new(url: params[:url],
-                  title: params[:title])
-    tag  = Tag.create(name: params[:tags])
-    link.tags << tag
+    link = Link.create(url: params[:url], title: params[:title])
+    params[:tags].split.each do |tag|
+      link.tags << Tag.create(name: tag)
+    end
     link.save
     redirect to('/links')
+  end
+
+  get '/tags/:name' do
+    tag = Tag.first(name: params[:name])
+    @links = tag ? tag.links : []
+    erb :'links/index'
   end
 
   run! if app_file == $0
--- spec/features/adding_tags_spec.rb
+++ spec/features/adding_tags_spec.rb
@@ -10,4 +10,15 @@ feature 'Adding tags' do
     link = Link.first
     expect(link.tags.map(&:name)).to include('education')
   end
+
+  scenario 'I can add multiple tags to a new link' do
+    visit '/links/new'
+    fill_in 'url',   with: 'http://www.testlink.com'
+    fill_in 'title', with: 'Test Links'
+
+    fill_in 'tags', with: 'education ruby'
+    click_button 'Create Link'
+    link = Link.first
+    expect(link.tags.map(&:name)).to include('education', 'ruby')
+  end
 end
