d3fd4fe6a7ee89c91ce3af957b9be187223d939c
Jonathan Pickard <Jonathandpickard@gmail.com>
2016-02-14 17:11:18 +0000

    finished 19

--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -3,7 +3,11 @@ require_relative '../web_helper'
 feature 'User sign up' do
   scenario 'I can sign up a new user' do
     expect { sign_up }.to change(User, :count).by(1)
-    expect(page).to have_content('Welcome fake_email@email.com')
-    expect(User.first.email).to eq('fake_email@email.com')
+    expect(page).to have_content('Welcome, test@example.com')
+    expect(User.first.email).to eq('test@example.com')
+  end
+
+  scenario 'It requires a matching confirmation password' do
+    expect { sign_up(password_confirmation: 'Wrong') }.not_to change(User, :count)
   end
 end
--- spec/web_helper.rb
+++ spec/web_helper.rb
@@ -1,7 +1,9 @@
-def sign_up
+def sign_up(email: 'test@example.com',
+            password: '123456',
+            password_confirmation: '123456')
   visit '/users/new'
-  expect(page.status_code).to eq(200)
-  fill_in :email, with: 'fake_email@email.com'
-  fill_in :password, with: 'fake_password'
+  fill_in :email, with: email
+  fill_in :password, with: password
+  fill_in :password_confirmation, with: password_confirmation
   click_button 'Sign up'
 end
